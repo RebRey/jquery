c3506db6184174325e9c14b7ca0c602cfd7f7553
Rebecca Reyes <rebreyaz@gmail.com>
2022-07-05 23:49:29 -0700

    initial check in

---
+++ index.js
@@ -0,0 +1,10 @@
+// instead of the $ sign you could type jquery instead
+// select the H1 HTML tag by using jquery or $
+// change the CSS color property to red
+// document.querySelector("h1").css("color", "red");
+// OR
+// $("h1").css("color", "red");
+
+//document.querySelectorAll("button");
+// OR
+// $("button");
